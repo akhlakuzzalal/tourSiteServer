bc174d128781d80950734d7cf63e3078300882ff
Akhlak Uzzalal <akhlakuzzalal@gmail.com>
2023-04-16 21:48:58 +0600

    add email validator

--- src/controler/user.controler.js
+++ src/controler/user.controler.js
@@ -1,8 +1,12 @@
 const { ERROR, OK } = require("../utils/responseHelper");
 const UserService = require("../service/userService");
+const { checkValidEmail } = require("../functions/common");
 
 exports.createUser = async (req, res) => {
   try {
+    const { email } = req.body;
+    const validEmail = checkValidEmail(email);
+    if (!validEmail) return ERROR(res, [], "Invalid email Address");
     const user = await UserService.createUser(req.body);
     if (user.message) return ERROR(res, [], user.message);
     return OK(res, user, "User created Successfully");
--- src/routes/api/user.api.js
+++ src/routes/api/user.api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const UserControler = require("../../controler/user.controler");
 
-router.post("/create", UserControler.createUser);
+router.post("/signUp", UserControler.createUser);
 router.get("/:email", UserControler.getUser);
 
 module.exports = router;
